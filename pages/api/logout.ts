9a77c2dace72469507b05ef3d8a4798935834feb
Elijah <elijah@stytch.com>
2021-10-05 16:15:00 -0700

    removed references to iron sessions

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,16 +1,12 @@
 // This API route logs a user out.
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Session } from 'next-iron-session';
 import { serialize } from 'cookie';
-
-// import withSession from '../../lib/withSession';
-type NextIronRequest = NextApiRequest & { session: Session };
 
 type Data = {
   error: string;
 };
 
-export async function handler(req: NextIronRequest, res: NextApiResponse<Data>) {
+export async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (req.method === 'POST') {
     try {
       // Set session
