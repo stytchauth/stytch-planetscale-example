0a25ddc6ca8e182b3e4a7e9f4c08876d52ade938
Elijah <elijah@stytch.com>
2021-10-06 12:17:31 -0700

    pulled stytch apis from main

--- pages/api/authenticate_magic_link.ts
+++ pages/api/authenticate_magic_link.ts
@@ -1,6 +1,6 @@
-// This API route authenticates a Stytch magic link.
 import type { NextApiRequest, NextApiResponse } from 'next';
 import loadStytch from '../../lib/loadStytch';
+import { serialize } from 'cookie';
 
 type Data = {
   error: string;
@@ -15,6 +15,11 @@ export async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
       //authenticate request and create 1 hour session
       const resp = await client.magicLinks.authenticate(token as string, { session_duration_minutes: 60 });
 
+      //send user to profile with cookies in response
+      res.setHeader(
+        'Set-Cookie',
+        serialize(process.env.COOKIE_NAME as string, resp.session_token as string, { path: '/' }),
+      );
       res.redirect('/profile');
       return;
     } catch (error) {
