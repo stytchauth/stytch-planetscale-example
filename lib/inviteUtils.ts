0618bcc4285c53c1551ba625dcf257021b7081f6
Elijah <elijah@stytch.com>
2021-10-06 13:29:29 -0700

    pulled from elijah

---
+++ lib/inviteUtils.ts
@@ -0,0 +1,15 @@
+import { BASE_URL } from './constants';
+
+const url = `${BASE_URL}/api/invite_user`;
+
+export async function inviteUser(email: string) {
+  const resp = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: email,
+    }),
+  });
+
+  const data = await resp.json();
+  return data;
+}
