aeab43b944d3be9b70f8abd63baccf8baccbf14f
Elijah <elijah@stytch.com>
2021-10-07 10:43:09 -0700

    refactored apis

--- lib/usersUtils.ts
+++ lib/usersUtils.ts
@@ -1,4 +1,45 @@
-export const signOut = async () => {
+import { User } from '../pages/api/users/';
+import { BASE_URL } from './constants';
+
+export async function getUserById(id: number) {
+  const resp = await fetch(`${BASE_URL}/api/users/${id}`, {
+    method: 'GET',
+  });
+  const data = await resp.json();
+  return data;
+}
+
+export async function deleteUserById(id: number) {
+  const resp = await fetch(`${BASE_URL}/api/users/${id}`, {
+    method: 'DELETE',
+  });
+  const data = await resp.json();
+  return data;
+}
+
+export async function addUser(name: string, email: string) {
+  const resp = await fetch(`${BASE_URL}/api/users`, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  });
+  const data = await resp.json();
+  return data;
+}
+
+export const getUsers = async (token: string): Promise<Array<User>> => {
+  // we pass the token in because redirects do not immediately update the cookie
+  //getUsers is the only functi being used with a redirect
+  const resp = await fetch(`${BASE_URL}/api/users?token=${token}`, {
+method: 'GET',
+  });
+  const data = await resp.json();
+  return data;
+};
+
+export const logout = async () => {
   const resp = await fetch('/api/logout', { method: 'POST' });
   const data = await resp.json();
   return data;
