fb940c37c7ce18321c706a4c77e19103e512baf2
Elijah <elijah@stytch.com>
2021-10-11 22:43:41 -0700

    destroying session token

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { serialize } from 'cookie';
+import {STYTCH_SESSION_NAME} from '../../lib/constants'
 
 type Data = {
   error?: string;
@@ -11,7 +12,9 @@ export async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
       //  you can add logic to destroy other sessions or cookies as well
       res
         .status(200)
-        .setHeader('Set-Cookie', serialize(process.env.COOKIE_NAME as string, '', { path: '/', maxAge: -1 }));
+        .setHeader('Set-Cookie', [
+          serialize(process.env.COOKIE_NAME as string, '', { path: '/', maxAge: -1 }),
+          serialize(STYTCH_SESSION_NAME, '',  { path: '/', maxAge: -1 })]);
     } catch (error) {
       res.status(400).json({ error });
     }
